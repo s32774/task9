2f252c5a4cb8a7b1ab89e684590f7b1e312fce83
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-05-30 17:38:33 +0200

    add view models

---
+++ Task9/VModels/NoteViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Task9.VModels;
+using System.ComponentModel.DataAnnotations;
+
+
+public class NoteViewModel
+{
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    public string Content { get; set; } = string.Empty;
+}
