72e2578c753446d3506b2e3c2dd49af5c41a7508
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-05-30 17:31:34 +0200

    first

---
+++ Task9/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Task9.Data;
+using Microsoft.EntityFrameworkCore;
+using Task9.Models;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> AppUsers => Set<AppUser>();
+    public DbSet<UserNote> UserNotes => Set<UserNote>();
+}
---
+++ Task9/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Task9.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=userpanel.db"));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
+app.Run();
