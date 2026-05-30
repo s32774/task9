0b1d7034c8e338f1aa1a9ddb8ade88891e58fccd
Olena Chornobai <s32774@pjwstk.edu.pl>
2026-05-30 17:42:19 +0200

    add admin controller

---
+++ Task9/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿namespace Task9.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Task9/Controllers/DashboardController.cs
@@ -0,0 +1,52 @@
+﻿namespace Task9.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Task9.Data;
+using Task9.Models;
+using Task9.VModels;
+
+[Authorize]
+public class DashboardController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public DashboardController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        var notes = await _context.UserNotes
+            .Where(n => n.AppUserId == userId)
+            .OrderByDescending(n => n.CreatedAt)
+            .ToListAsync();
+
+        return View(notes);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddNote(NoteViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return RedirectToAction("Index");
+
+        var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        var note = new UserNote
+        {
+            AppUserId = userId,
+            Title = model.Title,
+            Content = model.Content
+        };
+
+        _context.UserNotes.Add(note);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
+    }
+}
